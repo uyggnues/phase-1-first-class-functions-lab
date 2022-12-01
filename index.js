c1de308c110e2076a909afa3dfc87fb3db29d2e8
Seunggyu <seunggyul14@icloud.com>
2022-11-30 20:18:12 -0800

    commit tree b093e590cdd9834afe6c34bcb686bd79038654a1

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return () => {
+       return fare * fare
+    }
+    
+}
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers);
+   
+   
+   
+   
+   
+   
+   
+   
+    // if (selectingDrivers == returnFirstTwoDrivers) {
+    //     return returnFirstTwoDrivers(drivers);
+    // } else if (selectingDrivers == returnLastTwoDrivers) {
+    //     return returnLastTwoDrivers(drivers);
+    // }
+    
+}
+   
+
+
